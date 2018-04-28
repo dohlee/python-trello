911f2adec3b7a5e4ac07496d1e3c8bb7d5863b69
dohlee <apap950419@gmail.com>
2018-04-29 01:45:54 +0900

    Initial implementation of subcommands 'configure', 'comment', 'done'.

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from __future__ import print_function
 import io
 import re
 from glob import glob
+from os import walk
 from os.path import basename
 from os.path import dirname
 from os.path import join
@@ -13,6 +14,12 @@ from os.path import splitext
 
 from setuptools import find_packages
 from setuptools import setup
+
+
+# Adopted from http://thomas-cokelaer.info/blog/2012/03/how-to-embedded-data-files-in-python-using-setuptools/
+data_dir = 'share'
+data_files = [(d, [join(d, f) for f in files])
+              for d, directories, files in walk(data_dir)]
 
 
 def read(*names, **kwargs):
@@ -39,6 +46,7 @@ setup(
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     include_package_data=True,
     zip_safe=False,
+    data_files=data_files,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 5 - Production/Stable',
@@ -62,6 +70,7 @@ setup(
     ],
     install_requires=[
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
+        'requests==2.18.4',
     ],
     extras_require={
         # eg:
--- src/pytrello/cli.py
+++ src/pytrello/cli.py
@@ -15,12 +15,81 @@ Why does this file exist, and why not put this in __main__?
   Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
 """
 import argparse
+import json
+import os
+import pytrello
+import pytrello.api.card as card_api
 
-parser = argparse.ArgumentParser(description='Command description.')
-parser.add_argument('names', metavar='NAME', nargs=argparse.ZERO_OR_MORE,
-                    help="A name of something.")
+
+# Create the top-level parser.
+parser = argparse.ArgumentParser(description='Python wrapper for Trello API.')
+subparsers = parser.add_subparsers(description='Commands', dest='subcommand')
+
+
+# Adopted from https://mike.depalatis.net/blog/simplifying-argparse.html.
+def argument(*name_or_flags, **kwargs):
+    return ([*name_or_flags], kwargs)
+
+
+def subcommand(args=[], parent=subparsers):
+    def decorator(func):
+        parser = parent.add_parser(func.__name__, description=func.__doc__)
+        for arg in args:
+            parser.add_argument(*arg[0], **arg[1])
+        parser.set_defaults(func=func)
+    return decorator
+
+
+@subcommand()
+def configure(args):
+    """Configure pytrello with Trello API key and token.
+    """
+    package_dir = os.path.dirname(pytrello.__path__[0])
+    config_json = os.path.join(package_dir, '..', 'share', 'config.json')
+    config_dict = dict()
+
+    config_dict['key'] = input('Your Trello API key: ')
+    config_dict['token'] = input('Your Trello API token: ')
+
+    with open(config_json, 'w') as outFile:
+        print(json.dumps(config_dict), file=outFile)
+
+
+@subcommand([
+    argument('-b', '--board', nargs='+', required=True),
+    argument('-c', '--card', nargs='+', required=True),
+    argument('-i', '--comment', nargs='+', default=None),
+])
+def comment(args):
+    """Add comment to your trello card.
+    """
+    board_name = ' '.join(args.board)
+    card_name = ' '.join(args.card)
+    comment_ = ' '.join(args.comment) if args.comment is not None else None
+
+    card_id = card_api.get_card_id(board_name, card_name)
+    card_api.add_comment(card_id, comment_)
+
+
+@subcommand([
+    argument('-b', '--board', nargs='+', required=True),
+    argument('-c', '--card', nargs='+', required=True),
+    argument('-i', '--comment', nargs='+', default=None),
+])
+def done(args):
+    """Mark your trello card as done.
+    """
+    board_name = ' '.join(args.board)
+    card_name = ' '.join(args.card)
+    comment_ = ' '.join(args.comment) if args.comment is not None else None
+
+    card_id = card_api.get_card_id(board_name, card_name)
+    card_api.mark_as_done(card_id, comment_)
 
 
 def main(args=None):
     args = parser.parse_args(args=args)
-    print(args.names)
+    try:
+        args.func(args)
+    except AttributeError:
+        parser.print_help()
