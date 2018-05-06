fd98b357bdf8c1df7924317686916540d03773b9
dohlee <apap950419@gmail.com>
2018-05-07 00:33:40 +0900

    Bump version: 0.1.5 → 0.1.6

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def read(*names, **kwargs):
 
 setup(
     name='pytrello',
-    version='0.1.5',
+    version='0.1.6',
     license='MIT license',
     description='Python wrapper for Trello API.',
     long_description='%s\n%s' % (
